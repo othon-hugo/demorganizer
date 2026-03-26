d1564e100bcc577922ab3f0e35759f3c03758e51
othon <162609004+othonhugo@users.noreply.github.com>
2026-03-26 19:39:17 +0000

    refactor: rename `morgan` package to `algo`

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 from algebra import Variable
-from format import create_truth_table
-from morgan import (
+from algo import (
     apply_annihilation_law,
     apply_binary_constant_folding,
     apply_double_negation,
@@ -9,10 +8,9 @@ from morgan import (
     apply_inverse_law,
     apply_unary_constant_folding,
 )
+from format import create_truth_table
 
 __all__ = [
-    "Variable",
-    "create_truth_table",
     "apply_annihilation_law",
     "apply_binary_constant_folding",
     "apply_double_negation",
@@ -20,4 +18,6 @@ __all__ = [
     "apply_identity_law",
     "apply_inverse_law",
     "apply_unary_constant_folding",
+    "create_truth_table",
+    "Variable",
 ]
